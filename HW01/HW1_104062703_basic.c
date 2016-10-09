25b73ba9ce51cab918dbdaca783b3308d1290658
lubebul <rubys88684@gmail.com>
2016-10-09 16:05:28 +0800

    update

--- HW01/HW1_104062703_basic.c
+++ HW01/HW1_104062703_basic.c
@@ -3,17 +3,19 @@
 #include <assert.h>
 #include <mpi.h>
 
+#define max(x,y) (x)>(y)?(x):(y)
+
 void openFiles(char* in, char* out);
 void closeFiles(void);
 void printNums(int len, int* nums);
 void swap (int *nums, int i, int j);
-int* oddEvenSort(int len, int *nums);
+int* oddEvenSort(int len, int *nums, int rank, int chunk);
 
 int main (int argc, char** argv) {
   int rank, size;
   MPI_Init (&argc,&argv); MPI_Comm_size(MPI_COMM_WORLD, &size); MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   printf("size = %d, rank = %d\n", size, rank);
-  MPI_File *fin, *fout;
+  MPI_File fin, fout;
   // dealing with I/Os
   assert(("Usage: ./HW_104062703_basic N in-file out-file\n") && argc == 4);
   MPI_File_open(MPI_COMM_WORLD, argv[2], MPI_MODE_RDWR, MPI_INFO_NULL, &fin);
@@ -23,11 +25,14 @@ int main (int argc, char** argv) {
   MPI_Status suc;
   MPI_File_read(fin, nums, N, MPI_INT, &suc);
   printNums(N, nums);
-  nums = oddEvenSort(N, nums);
+  // sort
+  int chunk = (N+size-1) / size;
+  nums = oddEvenSort(N, nums, rank, chunk);
+  MPI_Barrier(MPI_COMM_WORLD);
   printNums(N, nums);
   // write sorted array
   MPI_File_write(fout, nums, N, MPI_INT, &suc);
-  MPI_File_close(fin); MPI_File_close(fout);
+  MPI_File_close(&fin); MPI_File_close(&fout);
   MPI_Finalize ();
   return 0;
 }
@@ -48,12 +53,14 @@ void swap (int *nums, int i, int j) {
   nums[i] = nums[j];
   nums[j] = tmp;
 }
-int* oddEvenSort(int len, int *nums) {
+int* oddEvenSort(int len, int *nums, int rank, int chunk) {
   int sorted = 0; int i;
+  int st = rank * chunk;
+  int ed = max(st + chunk -1, len-1);
   while (!sorted) {
     sorted = 1;
-    for (i=1; i<len-1; i+=2) if (nums[i] > nums[i+1]) { swap(nums, i, i+1); sorted = 0; }
-    for (i=0; i<len-1; i+=2) if (nums[i] > nums[i+1]) { swap(nums, i, i+1); sorted = 0; }
+    for (i=st+1; i<ed; i+=2) if (nums[i] > nums[i+1]) { swap(nums, i, i+1); sorted = 0; }
+    for (i=st; i<ed; i+=2) if (nums[i] > nums[i+1]) { swap(nums, i, i+1); sorted = 0; }
   }
   return nums;
 }
