b8fc6b172360151bf916ffb5dcb22a9c6f881a79
mrchen <you@example.com>
2016-10-17 23:55:44 +0800

    add testcase

---
+++ testcase/submit10.sh
@@ -0,0 +1,7 @@
+#PBS -N judge_sh_out
+#PBS -r n
+#PBS -l nodes=3:ppn=12
+#PBS -l walltime=00:01:00
+
+cd $PBS_O_WORKDIR
+mpiexec ./$exe 123457 testcase/testcase10 judge_out_10
---
+++ testcase/submit8.sh
@@ -0,0 +1,7 @@
+#PBS -N judge_sh_out
+#PBS -r n
+#PBS -l nodes=3:ppn=6
+#PBS -l walltime=00:01:00
+
+cd $PBS_O_WORKDIR
+mpiexec ./$exe 65537 testcase/testcase8 judge_out_8
