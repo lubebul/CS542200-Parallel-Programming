38b1c080a97a68a3ec9fe812eb42b5ac627f5932
lubebul <rubys88684@gmail.com>
2016-11-15 01:38:54 +0800

    fix exe bug

--- HW02/execute.py
+++ HW02/execute.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 NAME = ['MS_MPI_static',
         'MS_MPI_dynamic',
@@ -6,19 +7,44 @@ NAME = ['MS_MPI_static',
         'MS_OpenMP_dynamic',
         'MS_Hybrid_static',
         'MS_Hybrid_dynamic']
-FILE = '#PBS -N HYBRID\n#PBS -r n\n#PBS -l nodes={}:ppn={}\n5#PBS -l walltime=00:05:00\n#PBS -o {}\ncd $PBS_O_WORKDIR\nexport MV2_ENABLE_AFFINITY=0\n{}'
+FILE = '#PBS -N HYBRID\n#PBS -r n\n#PBS -l nodes={}:ppn={}\n#PBS -l walltime=00:05:00\n#PBS -o {}\ncd $PBS_O_WORKDIR\nexport MV2_ENABLE_AFFINITY=0\n{}'
 
 CMD = 'time mpiexec -ppn {} ./{} {} -2 2 -2 2 1000 1000 disable'
 
 def test(name):
     os.system('make {}'.format(name))
+    sent, count = 0, 0
+    
     for node in [1, 2, 3, 4]:
         for proc in [1, 2, 4, 8, 12]:
             job = FILE.format(node, proc, '{}_{}_{}.txt'.format(name, node, proc), CMD.format(node, name, proc))
             jobname = 'job_{}_{}_{}.txt'.format(name, node, proc)
             with open(jobname, 'w+') as f:
                 f.write(job)
+            # check current executed jobs
+            for f in os.listdir('../'):
+                if 'HYBRID' in f:
+                    count += 1
+            count /= 2
+            while sent-count > 7:
+                # check current executed jobs
+                for f in os.listdir('../'):
+                    if 'HYBRID' in f:
+                        count += 1
+                count /= 2
+                time.sleep(5)
             os.system('qsub {}'.format(jobname))
+            sent += 1
+    # collect into 1 file
+    cts = ''
+    for f in sorted(os.listdir('../')):
+        if 'HYBRID' in f:
+            with open(f, 'r') as fin:
+                data = fin.read()
+            cts = '{}\n{}'.format(cts, data)
+    with open('{}.txt'.format(name), 'w+') as f:
+        f.write(cts)
+    os.system('rm HYBRID.* job_*')
     os.system('make clean')
 for name in NAME:
     test(name)
