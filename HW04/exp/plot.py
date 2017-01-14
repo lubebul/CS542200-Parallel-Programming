dd9a3f3a058595a65c1f7b4bb1259be72be16d6f
lubebul <rubys88684@gmail.com>
2017-01-15 02:35:07 +0800

    plot exps

--- HW04/exp/plot.py
+++ HW04/exp/plot.py
@@ -14,12 +14,14 @@ def plot_weak(name):
     T1s = [float(x) for x in re.findall('\[I\d* \d*\]\n(.*) .* .*', data)]
     T2s = [float(x) for x in re.findall('\[I\d* \d*\]\n.* (.*) .*', data)]
     T3s = [float(x) for x in re.findall('\[I\d* \d*\]\n.* .* (.*)', data)]
+    """
     if 'cuda' not in name:
         Ns += Ns
         Bs += Bs
         T1s += [float(x) for x in re.findall('\[I\d* \d*\]\n.* .* .*\n(.*) .* .*', data)]
         T2s += [float(x) for x in re.findall('\[I\d* \d*\]\n.* .* .*\n.* (.*) .*', data)]
         T3s += [float(x) for x in re.findall('\[I\d* \d*\]\n.* .* .*\n.* .* (.*)', data)]
+    """
     df = pd.DataFrame({'N':Ns, 'B':Bs, 'computation':T1s, 'communication':T2s, 'memcpy':T3s})
     bar = Bar(df, label=['N','B'], values=blend('computation','communication','memcpy', name='times', labels_name='time'), xlabel='#node', ylabel='total time(s)', stack=cat(columns=['time'], sort=False), title='Weak Scalability')
     output_file('weak_{}.html'.format(name))
@@ -38,8 +40,9 @@ def plot_opt(name):
     T3 += [float(x) for x in re.findall('\[I\d* \d*\]\n.* .* .*\n.* .* (.*)', data)]
     df = pd.DataFrame({'type':TP, 'computation':T1, 'communication':T2, 'memcpy':T3})
     bar = Bar(df, label=['type'], values=blend('computation','communication','memcpy', name='times', labels_name='time'), xlabel='comparison', ylabel='total time(s)', stack=cat(columns=['time'], sort=False), title='{}'.format(name))
-    output_file('{}.html'.format(name))
+    output_file('{}.html'.format(name), title='N={}, B={}'.format(N,B))
     save(bar)
 for name in ['HW4_cuda', 'HW4_openmp', 'HW4_mpi']:
     plot_weak(name)
 plot_opt('share-memory')
+plot_opt('streaming')
